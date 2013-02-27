94de3c53688b1ebc4d47b3a3b8ec95f742825041
ichuan <iyanchuan@gmail.com>
2013-02-27 15:08:46 +0800

    vendorjs in dev update

--- js/vendor.js
+++ js/vendor.js
@@ -1,1 +1,5 @@
-document.write('<script src="../vendor/require/require.js"></script>');
+(function() {
+    var my_url = document.getElementById('_id-js-loader_').src,
+        dirname = my_url.substr(0, my_url.lastIndexOf('/') + 1);
+    document.write('<script src="' + dirname + '../vendor/require/require.js"></script>');
+})();
